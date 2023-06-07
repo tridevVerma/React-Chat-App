6777aa98a1f4035fee7598396db650db8b132cd3
Tridev Verma <tv072000@gmail.com>
2023-06-07 11:47:40 +0530

    Add sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -23,9 +23,45 @@ const Sidebar = () => {
 
       <div className="search-box">
         <form action="" method="get" spellCheck="false">
-          <i class="fa-solid fa-magnifying-glass"></i>
+          <i className="fa-solid fa-magnifying-glass"></i>
           <input type="text" placeholder="Search or start new chat..." />
         </form>
+      </div>
+
+      <div className="recent-chats">
+        <ul>
+          <li>
+            <div className="profile-image">
+              <img src="https://robohash.org/impeditautest.png" alt="" />
+            </div>
+            <div className="content">
+              <h4>Tridev Verma</h4>
+              <small>Hey!! whats going on ?</small>
+            </div>
+
+            <div className="status">
+              <small>10:12</small>
+              <span className="active"></span>
+            </div>
+          </li>
+          <li>
+            <div className="profile-image">
+              <img src="https://robohash.org/cupiditatererumquos.png" alt="" />
+            </div>
+            <div className="content">
+              <h4>Saurav Verma</h4>
+              <small>
+                Hii Everyone !! sdkfjsldkfj lksdjflkjsdlkfs sdflkjsdlkfjsdkfdsf
+                sdf sd f
+              </small>
+            </div>
+
+            <div className="status">
+              <small>10:40</small>
+              <span className=""></span>
+            </div>
+          </li>
+        </ul>
       </div>
     </StyledSidebar>
   );
--- src/styles/StyledSidebar.styles.js
+++ src/styles/StyledSidebar.styles.js
@@ -3,10 +3,14 @@ import styled from "styled-components";
 const StyledSidebar = styled.div`
   width: 100%;
   height: 100%;
-  padding: 1.5rem;
   background: var(--sidebar-bg);
+  display: grid;
+  grid-template-rows: auto auto 1fr;
 
   > .user-info {
+    width: 100%;
+    padding: 1.5rem;
+    padding-bottom: 0.8rem;
     display: flex;
     align-items: center;
     justify-content: space-between;
@@ -48,7 +52,9 @@ const StyledSidebar = styled.div`
   }
 
   > .search-box {
-    margin: 1.5rem auto;
+    width: 100%;
+    padding: 0rem 1.5rem;
+    margin: 1rem auto;
     > form {
       position: relative;
       > i {
@@ -65,6 +71,90 @@ const StyledSidebar = styled.div`
 
         border-radius: 2rem;
         box-shadow: var(--input-shadow);
+      }
+    }
+  }
+
+  > .recent-chats {
+    width: 100%;
+    max-height: 74vh;
+    overflow-y: scroll;
+
+    &::-webkit-scrollbar-track {
+      background-color: transparent;
+      border-radius: 10px;
+    }
+
+    &::-webkit-scrollbar {
+      width: 5px;
+      background-color: transparent;
+    }
+
+    &::-webkit-scrollbar-thumb {
+      border-radius: 5px;
+      background: hsla(199, 80%, 50%, 0.8);
+      backdrop-filter: blur(5px);
+      -webkit-backdrop-filter: blur(5px);
+    }
+
+    > ul {
+      > li {
+        padding: 1rem 0rem;
+        box-shadow: 0px 2px 2px -2px rgba(255, 255, 255, 0.3);
+        color: var(--body-text);
+        display: grid;
+        grid-template-columns: auto 1fr auto;
+        grid-gap: 1rem;
+        transition: all 0.25s ease-in-out;
+        &:hover {
+          background: linear-gradient(
+            to right,
+            rgba(155, 155, 155, 0.2),
+            rgba(155, 155, 155, 0.1)
+          );
+        }
+        > .profile-image {
+          margin-left: 1.5rem;
+          width: 50px;
+          aspect-ratio: 1 / 1;
+          border-radius: 50%;
+          padding: 0.4rem;
+          background: var(--icon-bg);
+          overflow: hidden;
+          > img {
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+          }
+        }
+
+        > .content {
+          small {
+            width: 15rem;
+            margin-top: 0.5rem;
+            display: block;
+            white-space: nowrap;
+            overflow: hidden;
+            text-overflow: ellipsis;
+          }
+        }
+
+        > .status {
+          margin-right: 1.5rem;
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          justify-content: space-evenly;
+          > span {
+            &:nth-child(2).active {
+              display: block;
+              width: 8px;
+              height: 8px;
+              border-radius: 50%;
+              background: hsl(110, 50%, 40%);
+            }
+          }
+        }
       }
     }
   }
